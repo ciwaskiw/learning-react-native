befcf310cf91e39e269e3bc054374d5bcbc483ed
Chris <christopher.iwaskiw@gmail.com>
2018-08-19 19:52:20 -0400

    Added a button to switch between two seperate pages

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isMain: true };
+  }
+
+  changePage = () => {
+    this.setState(
+      prev => {
+        return ({
+          isMain: !prev.isMain
+        })
+      }
+    )
+}
+
   render() {
-    return (
-      <View style={styles.container}>
+      if(this.state.isMain) {
+      return (<View style={styles.container}>
+        <Button
+          onPress={this.changePage}
+          title="Switch Pages"
+          color="#841584"
+          accessibilityLabel="Learn!"
+        />
         <Text style={{ color: '#fff'}}>Welcome to Chris Iwaskiw app #1!!!!</Text>
-        <Image   
-        	style={{width: 400, height: 400}}       
+        <Text style={{ color: '#fff'}}>Please make yourself at home...</Text>
+        <Text style={{ color: '#fff'}}>{this.state.isMain.toString()}</Text>
+        <Image
+        	style={{width: 400, height: 400}}
           source={{uri: 'https://i.imgur.com/zY9gdHQ.jpg'}}
         />
-      </View>
-    );
+      </View>)
+    } else {
+      return (<View style={styles.container}>
+        <Button
+            onPress={this.changePage}
+          title="Switch Pages"
+          color="#841584"
+          accessibilityLabel="Learn!"
+        />
+        <Text style={{ color: '#fff'}}>This is an alternate page.</Text>
+        <Image
+        	style={{width: 400, height: 400}}
+          source={{uri: 'https://pbs.twimg.com/media/BoavZpDIAAAfiGO.png'}}
+        />
+      </View>)
+    }
   }
 }
 
